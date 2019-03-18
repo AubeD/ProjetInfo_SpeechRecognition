6fa510ba1bde303c81ec1b669518e1ca4e6596e7
NatanBT <natan.baron-trocellier@ecl15.ec-lyon.fr>
2019-03-18 16:24:04 +0100

    Ajout écoute de FP en continu et nettoyage de RIEN

--- sphinx_reglage_seuils_anti_surdetection.py
+++ sphinx_reglage_seuils_anti_surdetection.py
@@ -171,9 +171,59 @@ def visualiser_TP_FP():
     
     return TP_FP
 
-
+def enregistrer_FP_en_continu(nb_extraits, duree_enregistrement=5):
+    with sr.Microphone(sample_rate=44100) as source:
+        r = sr.Recognizer()
+        for i in range(nb_extraits):
+            print('Enregistrement {}/{} commencé'.format(i+1, nb_extraits))
+            audio = r.listen(source,phrase_time_limit=duree_enregistrement)
+            print('Enregistrement {}/{} terminé'.format(i+1, nb_extraits))
+            fichier_wav = audio.get_wav_data()     
+            chemin = 'records/RIEN'
+            if not(os.path.exists(chemin)):
+                os.makedirs(chemin)
+            nb_fichiers = len(os.listdir(chemin))
+            chemin_nouveau_fichier = '{}/{}_{}.wav'.format(chemin, 'RIEN_cours_enregistre', nb_fichiers+1)
+            with open(chemin_nouveau_fichier, 'wb') as fo:
+                fo.write(fichier_wav)
+                
+def supprimer_enregistrements_RIEN_sans_FP():
+    # Configuration de Sphinx
+    model_path = 'EN-NAO'
+    config = {
+        'hmm': os.path.join(model_path, 'acoustic-model'),
+        'lm': os.path.join(model_path, 'language-model.lm.bin'),
+        'dict': os.path.join(model_path, 'pronounciation-dictionary.dict'),
+        'grammar':os.path.join(model_path, 'GrammarNAO.jsgf')
+    }
+    config_sphinx = (config['hmm'], config['lm'], config['dict'])
+    grammar = config['grammar']
+    chemin = 'records/RIEN/'
+    fichiers_conserves = []
+    fichiers_supprimes = []
+    print('Début du nettoyage des fichiers inutiles...')
+    for fichier in os.listdir(chemin):
+        if analyser_son(chemin+fichier, config_sphinx, grammar)[1] == 'RIEN':
+            fichiers_supprimes.append(chemin+fichier)
+            print('Fichier {} supprimé !'.format(chemin+fichier))
+        else:
+            print('Fichier {} conservé !'.format(chemin+fichier))
+            fichiers_conserves.append(chemin+fichier)
+    for chemin_fichier in fichiers_supprimes:
+        os.remove(chemin_fichier)
+    for i in range(len(fichiers_conserves)):
+        os.rename(fichiers_conserves[i], '{}RIEN_FP_confirme_attente_{}'.format(chemin, i+1))
+    for i in range(len(fichiers_conserves)):
+        chemin_fichier = '{}RIEN_FP_confirme_attente_{}'.format(chemin, i+1)
+        chemin_slice = chemin_fichier.split('attente_')
+        os.rename(chemin_fichier, chemin_slice[0] + chemin_slice[1])
+    print('Nettoyage terminé !')
+            
         
 #while True:
 #    enregistrer_phrase()
 
-visualiser_TP_FP()
+#enregistrer_FP_en_continu(50, 5)
+#supprimer_enregistrements_RIEN_sans_FP()
+
+#visualiser_TP_FP()
