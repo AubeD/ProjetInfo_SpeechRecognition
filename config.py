366307ac3ce07e147fac26e8e516f22cd2103cc9
NatanBT <natan.baron-trocellier@ecl15.ec-lyon.fr>
2019-03-27 16:44:59 +0100

    Ajout du graphe d'états et transformation de la grammaire

--- config.py
+++ config.py
@@ -14,6 +14,10 @@ Created on Thu Mar 21 10:16:42 2019
 ###########################################################################################
 
 
+###########################################################################################
+# Chemins d'accès                                                                         #
+###########################################################################################
+
 # Module os, ici pour la gestion des chemins de fichiers
 import os
 
@@ -23,7 +27,7 @@ model_path = 'EN-NAO/'
 chemin_buffer_entendu = 'communication/buffer_entendu.txt'
 chemin_buffer_compris = 'communication/buffer_compris.txt'
 # Buffer de communication C++ -> Python
-chemin_stopfile = 'communication/stop_Python.txt'
+chemin_buffer_C = 'communication/buffer_C.txt'
 # Logs écrits par le fichier programme_python_nao.py
 chemin_pylog = 'logs/log_Python.log'
 
@@ -35,26 +39,62 @@ config_sphinx = {
     'grammar':os.path.join(model_path, 'GrammarNAO.jsgf')              # Fichier de grammaire utilisée
 }
 
-# Instructions à reconnaître et seuils de confiance nécessaires à leur conservation
-# (ces seuils sont déterminés par "clustering visuel" à l'aide de sphinx_reglage_seuils_anti_surdetection.py)
+########################################################################################################################
+# Instructions à reconnaître et seuils de confiance nécessaires à leur conservation                                    #
+# (ces seuils sont déterminés par "clustering visuel" à l'aide de sphinx_reglage_seuils_anti_surdetection.py)          #
+# Pour chaque instruction: seuils=  [s1, s2] => score <= s1: rejet, score >=s2: acceptation, s1<score<s2: acceptation  #
+# Si s1=s2=s: pas de domaine de doute, score <= s: rejet et score > s: acceptation                                     #
+########################################################################################################################
 instructions =  {
-                'connect nao':-6800,
-                'make nao rest':-9000,
-                'start real-time telecontrol':-11000,
-                'stop real-time telecontrol':-7500,
-                'mirror on':-4500,
-                'mirror off':-7500,
-                'connect kinect':-6500,
-                'close kinect':-4500,
-                'save movement':-6000,
-                'stop movement':-5500,
-                'start listening': -100000,
-                'stop listening': -100000,
-                'yes nao': -100000,
-                'no nao': -100000,
-                'RIEN':0 # 'RIEN' désigne une absence d'instruction reconnue.
-                }
+        'connect nao':                  [-6800, -6800],
+        'make nao rest':                [-8000, -8000],
+        'start real-time telecontrol':  [-10000, -10000],
+        'stop real-time telecontrol':   [-9500, -7000],
+        'mirror on':                    [-8000, -8000],
+        'mirror off':                   [-8000, -8000],
+        'connect kinect':               [-6000, -6000],
+        'close kinect':                 [-5000, -4500],
+        'save movement':                [-8000, -4000],
+        'stop movement':                [-7000, -5500],
+        'start listening':              [-100000, -100000],
+        'stop listening':               [-100000, -100000],
+        'yes nao':                      [-100000, -100000],
+        'no nao':                       [-100000, -100000],
+        'RIEN':                         [0, 0]  # 'RIEN' désigne une absence d'instruction reconnue.
+        }
 
-# Microphone à utiliser et son paramétrage (utiliser scripts_utilitaires/listage_micros.py pour le trouver)
+###########################################################################################
+# Graphe d'états de la reconnaissance vocale.                                             #
+###########################################################################################
+
+# Noeuds/états du graphe
+etats_possible_reconnaissance = ['stop', 'pause', 'doute', 'attente_instruction']
+
+# Instructions comprises par NAO lorsqu'on se trouve dans chaque noeud du graphe
+instructions_comprises = {
+        'arret': [],
+        'pause':['start listening'],
+        'doute':['yes nao', 'no nao'],
+        'attente_instruction': [instr for instr in instructions if not(instr in ['start listening', 'yes nao', 'no nao', 'RIEN'])]
+        }
+
+# Pour chaque état, liste des instructions causant le passage à un autre état
+transitions_vocales = {
+        'arret': {},
+        'pause': {'start listening': 'attente_instruction'},
+        'doute': {'yes nao': 'attente_instruction', 'no nao': 'attente_instruction'},
+        'attente_instruction': {'stop listening': 'pause'}
+        }
+
+# Pour chaque message envoyé par le code C++, transition d'un état à un autre induite
+transitions_provoquees = {
+        'stop':'arret',
+        'pause':'pause',
+        'reprise':'attente_instruction'
+        }
+
+#############################################################################################################
+# Microphone à utiliser et son paramétrage (utiliser scripts_utilitaires/listage_micros.py pour le trouver) #
+#############################################################################################################
 id_micro = None # None pour utiliser le périphérique par défaut
 sample_rate = 44100
--- programme_python_nao.py
+++ programme_python_nao.py
@@ -46,13 +46,13 @@ def write_log(texte):
     """ Écrit texte dans le fichier de log Python puis retourne à la ligne"""
     print_fichier(config.chemin_pylog, texte+'\n', False)
     
-def read_stopfile():
+def read_buffer_C():
     """
     Lit le buffer de communication C++ -> Python. Si ce dernier contient "stop",
     on arrêtera la reconnaissance vocale.
     """
-    with open(config.chemin_stopfile, 'r') as foo:
-        return foo.readline() == 'stop'
+    with open(config.chemin_buffer_C, 'r') as foo:
+        return foo.readline()
 
 def init_buffer(buffer):
     """
@@ -74,7 +74,80 @@ def write_buffer(buffer, texte):
     elif buffer=='compris par nao':
         print_fichier(config.chemin_buffer_compris, texte+'\n', False)
         
+ 
     
+def reconnaissance_vocale(mode_reconnaissance, r, source, config_sphinx, grammaire):
+    """
+    Effectue la reconnaissance vocale de la prochaine phrase à venir. Prend en paramètres:
+        - le mode de reconnaissance actuel
+        - l'objet sr.Recorder r permettant de segmenter et d'analyser un extrait audio avec Sphinx
+        - une source audio source (fichier .wav ou stream en sortie du micro de l'appareil)
+        - config_sphinx et grammaire, la configuration de la reconnaissance Sphinx
+    Filtre les instructions reconnues en fonction du mode de reconnaissance actuel.
+    Communique le cas échéant l'instruction qui a été reconnue au code C++, via les différents buffers.    
+    Retourne le mode de reconnaissance à la fin de la reconnaissance vocale (il diffère du mode de 
+    départ si l'instruction reconnue implique un changement de mode).
+    """    
+    
+    # Si C++ s'est arrêté, on ne cherche pas à reconnaître quoi que ce soit et on laisse Python
+    # s'arrêter.
+    if mode_reconnaissance == 'arret':
+        return 'arret'
+    
+    # Segmentation et enregistrement d'une phrase
+    audio = r.listen(source,phrase_time_limit=5)
+    try:
+        # On soumet la phrase à Sphinx et on récupère son objet-hypothèse
+        response = r.recognize_sphinx(audio, language=config_sphinx, grammar=grammaire, show_all=True)
+        hypothese = response.hyp()
+        if hypothese:
+            # Si du texte intelligible a été reconnu, et que c'est une instruction attendue
+            # dans le mode d'écoute actuel...
+            instruction = instruction_plus_proche(hypothese.hypstr)
+            write_buffer('entendu par nao', hypothese.hypstr)
+            if instruction in config.instructions_comprises[mode_reconnaissance]:
+                # On extrait le score de confiance associé à l'instruction
+                score = hypothese.best_score
+                # En fonction de ce dernier, on accepte ou non l'instruction, ou on
+                # se place en état de doute
+                score_rejet, score_acceptance = config.instructions[instruction]
+                write_log('{} vs [{}  {}]'.format(score, score_rejet, score_acceptance))
+                
+                if score <= score_rejet: # rejet comme FP
+                    write_log('{}: {} vs [{}  {}] =>Rejet comme Faux Positif'.format(instruction, score, score_rejet, score_acceptance))
+                    return mode_reconnaissance
+                
+                elif score >= score_acceptance: # acceptation comme TP
+                    write_buffer('compris par nao', '{}:{}'.format(instruction, 'OK'))
+                    write_log('{}: {} vs [{}  {}] =>Instruction transmise'.format(instruction, score, score_rejet, score_acceptance))
+                    # Si l'instruction reconnue correpond à une transition d'un état de la
+                    # reconnaissance vocale à un autre, on effectue cette transition
+                    if instruction in config.transitions_vocales[mode_reconnaissance]:
+                        return config.transitions_vocales[mode_reconnaissance][instruction]
+                    else:
+                        return mode_reconnaissance
+                    
+                else: # On se place en état de doute et on va demander à l'utilisateur son avis
+                    write_buffer('compris par nao', '{}:{}'.format(instruction, 'doute'))
+                    write_log('{}: {} vs [{}  {}] =>Doute de NAO'.format(instruction, score, score_rejet, score_acceptance))
+                    return 'doute'
+            else:
+                write_log('{}: instruction inconnue dans le mode {}'.format(instruction, mode_reconnaissance))
+                return mode_reconnaissance
+                    
+        else: # Si aucun texte n'a été reconnu on ne fait rien
+            write_buffer('entendu par nao', '')
+            return mode_reconnaissance
+        
+    # Si Sphinx renvoie une erreur, on continue la reconnaissance en modifiant le fichier de logs
+    # et on continue en conservant l'état actuel
+    except sr.UnknownValueError:
+        write_log("Sphinx could not understand audio")
+        return mode_reconnaissance        
+    except sr.RequestError as e:
+         write_log("Sphinx error; {0}".format(e))
+         return mode_reconnaissance
+         
 #########################################################################################
 # Script lancé en parallèle du code C++                                                 #
 #########################################################################################
@@ -89,35 +162,20 @@ init_log()
 init_buffer('entendu par nao')
 init_buffer('compris par nao')
 
+# Initialisation du mode de reconnaissance: on commence par écouter les instructions possibles
+mode_reconnaissance = 'attente_instruction' # modes possibles: 'attente_instruction', 'doute', 'pause', 'arret'
+
 # Initialisation de l'écoute et de l'objet de reconnaissance vocale
 with sr.Microphone(device_index=config.id_micro, sample_rate=config.sample_rate) as source:
     r = sr.Recognizer()
     r = sr.Recognizer()
     # Début de la reconnaissance vocale
-    while not(read_stopfile()):
-        # Segmentation et enregistrement d'une phrase
-        audio = r.listen(source,phrase_time_limit=5)
-        try:
-            # On soumet la phrase à Sphinx et on récupère son objet-hypothèse
-            response = r.recognize_sphinx(audio, language=config_sphinx, grammar=grammaire, show_all=True)
-            hypothese = response.hyp()
-            if hypothese:
-                # Si du texte intelligible a été reconnu, et que c'est une instruction connue
-                instruction = instruction_plus_proche(hypothese.hypstr)
-                write_buffer('entendu par nao', hypothese.hypstr)
-                write_log('Speech recognized: {}'.format(instruction))
-                if instruction != 'RIEN':
-                    # On récupère le couple (instruction, score). Si score > seuil(instruction) on la transmet
-                    # au programme C++ via le buffer Python->C++
-                    score = hypothese.best_score
-                    if score > config.instructions[instruction]:
-                        write_buffer('compris par nao', instruction)
-                    else:
-                        write_log('{}: Rejet comme Faux Positif'.format(instruction))
-            else:
-                write_buffer('entendu par nao', '')
-        # Si Sphinx renvoie une erreur, on continue la reconnaissance en modifiant le fichier de logs
-        except sr.UnknownValueError:
-            write_log("Sphinx could not understand audio")
-        except sr.RequestError as e:
-             write_log("Sphinx error; {0}".format(e))
+    while not(mode_reconnaissance == 'arret'):
+        # Si le code C envoie une instruction, on outrepasse le fonctionnement normal de la reconnaissance
+        # vocale et on se place à l'état indiqué (par exemple 'arret' pour mettre fin à la reconnaissance)
+        message_code_C = read_buffer_C()
+        if message_code_C:
+            mode_reconnaissance = config.transitions_provoquees[message_code_C]
+        # On reconnaît une phrase. Si elle correspond à une instruction possible dans le contexte du mode de
+        # reconnaissance actuel, on la transmet à C++ et on met à jour le mode de reconnaissance au besoin.
+        mode_reconnaissance = reconnaissance_vocale(mode_reconnaissance, r, source, config_sphinx, grammaire)
